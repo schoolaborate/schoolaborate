1648fd62f7e0d9b26273208a47a5ea8845e53f54
EazyCreator <anarvaezrs@gmail.com>
2022-05-05 13:39:33 -0400

    General / For you views added

---
+++ Schoolaborate/GeneralViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GeneralViewController.swift
+//  Schoolaborate
+//
+//  Created by Anthony Narvaez on 5/5/22.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var Navitem: UINavigationItem!
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
