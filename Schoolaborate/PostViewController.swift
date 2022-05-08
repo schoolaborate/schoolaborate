64fb0876115285f350c012ea27fdce5314620333
EazyCreator <anarvaezrs@gmail.com>
2022-05-07 21:53:52 -0400

    Post's can be created with image, caption, and author saved to Parse
    Post's can be created with image, caption, and author saved to Parse.
    
    Missing segue to home view controller after clicking submit, and deletion back to nil parameters after clicking submit.

---
+++ Schoolaborate/PostViewController.swift
@@ -0,0 +1,80 @@
+//
+//  PostViewController.swift
+//  Schoolaborate
+//
+//  Created by Anthony Narvaez on 5/7/22.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class PostViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = captionField.text
+        post["Author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { success, error in
+            if success{
+                print("saved!")
+            }
+                else{
+                    print("error!")
+                }
+            }
+        }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
